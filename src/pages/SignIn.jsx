1f8590537054a041172d95f723978f0ac22053f6
FerDeveloperJS <ferchosoftwaredev@gmail.com>
2025-11-26 07:02:08 -0500

    Added SignIn by Auth system On Supabase

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,44 @@
+import { supabase } from "../supabaseClient.js";
 import mind from "../assets/img/mind.png";
+import LoginNotification from "../components/LoginNotification";
+import { useState } from "react";
 
 function SignIn() {
+  const [showNotification, setShowNotification] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState("");
+  const [notificationBackground, setNotificationBackground] = useState("");
+
+  async function handleSignIn(email, password) {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) {
+        setShowNotification(true);
+        setNotificationMessage(error.message);
+        setNotificationBackground("bg-red-400");
+        return;
+      }
+
+      setShowNotification(true);
+      setNotificationMessage("Has iniciado sesión exitosamente");
+      setNotificationBackground("bg-green-400");
+    } catch (error) {
+      alert("Error inesperado:", error.message);
+    }
+  }
   return (
     <section className="bg-[#7B2CBF] min-h-screen p-2">
+      {showNotification && (
+        <LoginNotification
+          setShowNotification={setShowNotification}
+          message={notificationMessage}
+          bgColor={notificationBackground}
+        />
+      )}
+
       <img src={mind} className="-mt-5 w-36 md:w-52 lg:w-60 mx-auto" />
       <h1 className="-mt-5 text-3xl md:text-5xl lg:text-7xl text-center font-bold text-white font-[Onest]">
         Quiz App By Fer
@@ -37,7 +73,17 @@ function SignIn() {
           />
         </div>
 
-        <button className="w-48 h-10 md:w-80 lg:w-96 md:h-12 rounded-xl bg-[#10002B] text-white md:text-xl lg:text-2xl font-bold p-2 flex items-center justify-center">
+        <button
+          className="w-48 h-10 md:w-80 lg:w-96 md:h-12 rounded-xl bg-[#10002B] text-white md:text-xl lg:text-2xl font-bold p-2 flex items-center justify-center"
+          onClick={async (e) => {
+            e.preventDefault();
+
+            const emailValue = document.querySelector("#email").value;
+            const passwordValue = document.querySelector("#password").value;
+
+            await handleSignIn(emailValue, passwordValue);
+          }}
+        >
           Iniciar Sesión
         </button>
       </form>
