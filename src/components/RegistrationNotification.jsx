3857eb75c272ab7188cbdee97c875176bc7e838f
FerDeveloperJS <ferchosoftwaredev@gmail.com>
2025-11-26 06:40:18 -0500

    Added SignUp by Auth system On Supabase

---
+++ src/components/RegistrationNotification.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+function RegistrationNotification({ setShowNotification, message, bgColor }) {
+  useEffect(() => {
+    setTimeout(() => {
+      setShowNotification(false);
+    }, 4000);
+  }, []);
+
+  return (
+    <div
+      className={`absolute top-12 left-1/2 -translate-x-1/2 text-xl w-72 rounded-xl p-2 font-medium ${bgColor}`}
+    >
+      <p className="font-[Onest]">{message}</p>
+    </div>
+  );
+}
+
+export default RegistrationNotification;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,12 +1,52 @@
+import { supabase } from "../supabaseClient.js";
 import mind from "../assets/img/mind.png";
+import RegistrationNotification from "../components/RegistrationNotification.jsx";
+import { useState } from "react";
 
 function SignUp() {
+  const [showNotification, setShowNotification] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState("");
+  const [notificationBackground, setNotificationBackground] = useState("");
+
+  async function handleSignUp(email, password) {
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+
+      if (error) {
+        setShowNotification(true);
+        setNotificationMessage(error.message);
+        setNotificationBackground("bg-red-400");
+        return;
+      }
+
+      setShowNotification(true);
+      setNotificationMessage(
+        "Te has registrado exitosamente, revisa tu correo para confirmar"
+      );
+      setNotificationBackground("bg-green-400");
+    } catch (error) {
+      alert("Error inesperado:", error);
+    }
+  }
+
   return (
     <section className="bg-[#7B2CBF] min-h-screen p-2">
+      {showNotification && (
+        <RegistrationNotification
+          setShowNotification={setShowNotification}
+          message={notificationMessage}
+          bgColor={notificationBackground}
+        />
+      )}
       <img src={mind} className="-mt-5 w-36 md:w-52 lg:w-60 mx-auto" />
+
       <h1 className="-mt-5 text-3xl md:text-5xl lg:text-7xl text-center font-bold text-white font-[Onest]">
         Quiz App By Fer
       </h1>
+
       <form className="mt-10 flex flex-col items-center gap-10">
         <h2 className="text-white font-bold md:text-xl lg:text-2xl">
           Registrarse
@@ -48,8 +88,19 @@ function SignUp() {
           />
         </div>
 
-        <button className="w-48 h-10 md:w-80 lg:w-96 md:h-12 rounded-xl bg-[#10002B] text-white md:text-xl lg:text-2xl font-bold p-2 flex items-center justify-center">
-          Iniciar Sesión
+        <button
+          type="submit"
+          className="w-48 h-10 md:w-80 lg:w-96 md:h-12 rounded-xl bg-[#10002B] text-white md:text-xl lg:text-2xl font-bold p-2 flex items-center justify-center"
+          onClick={async (e) => {
+            e.preventDefault();
+
+            const emailValue = document.querySelector("#email").value;
+            const passwordValue = document.querySelector("#password").value;
+
+            await handleSignUp(emailValue, passwordValue);
+          }}
+        >
+          Registrarse
         </button>
       </form>
     </section>
